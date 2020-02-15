af89313fa9082474acddabec1542d1d28f20f84a
InJaEE <in11202@naver.com>
2020-02-15 19:04:25 +0900

    Typescript Generic

--- TIL/Typescript/hearthstone.js
+++ TIL/Typescript/hearthstone.js
@@ -1,33 +1,22 @@
 "use strict";
-// class Card{
-//     public att: number;
-//     public hp: number;
-//     private cost: number;
-// }
-class Card {
-    constructor(hero, mine) {
-        if (hero) {
-            return new Hero(mine);
-            // this.att = Math.ceil(Math.random() * 2);
-            // this.hp = Math.ceil(Math.random() * 5) + 25;
-            // this.hero = true;
-            // this.field = true;
-        }
-        else {
-            this.att = Math.ceil(Math.random() * 5);
-            this.hp = Math.ceil(Math.random() * 5);
-            this.cost = Math.floor((this.att + this.hp) / 2);
-        }
-    }
-}
-const cardTest = new Card(true, true);
-class Hero extends Card {
+// implements는 '구현한다'라고 생각하면 된다.
+// 실제로 사용하면 Class, 구현먼하면 interface(implements)
+class Hero {
     constructor(mine) {
-        super(true, mine);
+        this.mine = mine;
         this.att = Math.ceil(Math.random() * 2);
         this.hp = Math.ceil(Math.random() * 5) + 25;
         this.hero = true;
         this.field = true;
+    }
+}
+class Sub {
+    constructor(mine) {
+        this.field = false;
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 5);
+        this.hp = Math.ceil(Math.random() * 5);
+        this.cost = Math.floor((this.att + this.hp) / 2);
     }
 }
 const opponent = {
@@ -41,3 +30,46 @@ const opponent = {
     chosenCard: null,
     chosenCardData: null,
 };
+const me = {
+    hero: document.getElementById('rival-hero'),
+    deck: document.getElementById('rival-deck'),
+    field: document.getElementById('rival-cards'),
+    cost: document.getElementById('rival-cost'),
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null,
+};
+const turnButton = document.getElementById('turn-btn');
+let turn = true; // true: 내 턴, false: 상대 턴
+function initiate() {
+    [opponent, me].forEach((item) => {
+        item.deckData = [];
+        item.heroData = null;
+        item.fieldData = [];
+        item.chosenCard = null;
+        item.chosenCardData = null;
+    });
+    createDeck({ mine: false, count: 5 });
+    createDeck({ mine: true, count: 5 });
+    createHero({ mine: false });
+    createHero({ mine: true });
+    //redrawScreen({mine: true});
+    //redrawScreen({mine: false});
+}
+function createDeck({ mine, count }) {
+}
+function createHero({ mine }) {
+    const player = mine ? me : opponent;
+    player.heroData = new Hero(mine);
+}
+;
+const ab = {
+    add: (a, b) => a + b
+};
+const bc = {
+    add: (a, b) => a + b
+};
+['a', 3, true].forEach(item => {
+});
--- TIL/Typescript/hearthstone.ts
+++ TIL/Typescript/hearthstone.ts
@@ -1,47 +1,45 @@
-// class Card{
-//     public att: number;
-//     public hp: number;
-//     private cost: number;
-// }
-
-class Card{
-    protected att?: number;
-    protected hp?: number;
-    private cost?: number;
-    protected mine?: boolean;
-
-    constructor(hero: boolean, mine: boolean){
-        if(hero){
-            return new Hero(mine);
-            // this.att = Math.ceil(Math.random() * 2);
-            // this.hp = Math.ceil(Math.random() * 5) + 25;
-            // this.hero = true;
-            // this.field = true;
-        } else{
-            this.att = Math.ceil(Math.random() * 5);
-            this.hp = Math.ceil(Math.random() * 5);
-            this.cost = Math.floor((this.att + this.hp) / 2);
-        }
-    }
+interface Card{
+    att: number;
+    hp: number;
+    mine: boolean;
+    field: boolean;
+    cost?: number;
+    hero?: boolean;
 }
 
-const cardTest = new Card(true, true);
-
-class Hero extends Card{
-    private hero: boolean;
-    private field: boolean;
+// implements는 '구현한다'라고 생각하면 된다.
+// 실제로 사용하면 Class, 구현먼하면 interface(implements)
+class Hero implements Card{
+    public att: number;
+    public hp: number;
+    public hero : boolean;
+    public field: true;
+    public mine: boolean;
 
     constructor(mine: boolean){
-        super(true, mine);
+        this.mine = mine;
         this.att = Math.ceil(Math.random() * 2);
         this.hp = Math.ceil(Math.random() * 5) + 25;
         this.hero = true;
         this.field = true;
     }
-
 }
 
-interface Play{
+class Sub implements Card{
+    public att: number;
+    public hp: number;
+    public mine: boolean;
+    public field: boolean = false;
+    public cost: number;
+    constructor(mine: boolean){
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 5);
+        this.hp = Math.ceil(Math.random() * 5);
+        this.cost = Math.floor((this.att + this.hp) / 2);
+    }
+}
+
+interface Player{
     hero: HTMLDivElement,
     deck: HTMLDivElement,
     field: HTMLDivElement,
@@ -53,7 +51,19 @@ interface Play{
     chosenCardData: Card | null,
 }
 
-const opponent: Play = {
+const opponent: Player = {
+    hero: document.getElementById('rival-hero') as HTMLDivElement,
+    deck: document.getElementById('rival-deck') as HTMLDivElement,
+    field: document.getElementById('rival-cards') as HTMLDivElement,
+    cost: document.getElementById('rival-cost') as HTMLDivElement,
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null,
+}
+
+const me: Player = {
     hero: document.getElementById('rival-hero') as HTMLDivElement,
     deck: document.getElementById('rival-deck') as HTMLDivElement,
     field: document.getElementById('rival-cards') as HTMLDivElement,
@@ -64,3 +74,67 @@ const opponent: Play = {
     chosenCard: null,
     chosenCardData: null,
 };
+
+const turnButton = document.getElementById('turn-btn') as HTMLButtonElement;
+let turn = true; // true: 내 턴, false: 상대 턴
+
+function initiate(){
+    [opponent, me].forEach((item) => {
+        item.deckData = [];
+        item.heroData = null;
+        item.fieldData = [];
+        item.chosenCard = null;
+        item.chosenCardData = null;
+    });
+    createDeck({mine: false, count: 5});
+    createDeck({mine: true, count: 5});
+    createHero({mine: false});
+    createHero({mine: true});
+    //redrawScreen({mine: true});
+    //redrawScreen({mine: false});
+}
+
+function createDeck({mine, count}: {mine: boolean, count: number}){
+
+}
+
+function createHero({mine}: {mine: boolean}){
+    const player = mine ? me: opponent;
+    player.heroData = new Hero(mine);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 제네릭은 사용할때 타입을 정할 수 있게 해준다.
+interface obj<T extends string | number>{
+    add: (a: T, b: T) => T;
+    remove?: (a: T, b: T) => T;
+};
+
+const ab: obj<number> = {
+    add:(a, b) => a+b
+};
+
+const bc: obj<string> = {
+    add:(a, b) => a+b
+};
+
+['a', 3, true].forEach(item => {
+    
+});
